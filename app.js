2bf8205b01a583051c15e0a61b1d46ecb42459e5
Lucas Yang <yangchenshin77@gmail.com>
2022-12-17 19:19:33 +0800

    Add redis cache & LINE group support

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const express = require('express')
-const line = require('@line/bot-sdk')
+const { createClient: createRedisClient } = require('redis')
+const LINE = require('@line/bot-sdk')
 const { Configuration, OpenAIApi } = require('openai')
 const c = require('chalk')
 
@@ -16,22 +17,32 @@ const lineConfig = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 }
-const client = new line.Client(lineConfig)
+const linebot = new LINE.Client(lineConfig)
 
-// express instance
+// Redis instance
+const redis = createRedisClient({
+  url: process.env.REDIS_URL || 'redis://localhost:6379',
+})
+
+// Express instance
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/callback', line.middleware(lineConfig), (req, res) => {
+app.post('/callback', LINE.middleware(lineConfig), async (req, res) => {
+  await redis.connect()
+
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err)
       res.status(500).end()
+    })
+    .finally(async () => {
+      await redis.disconnect()
     })
 })
 
@@ -41,24 +52,54 @@ async function handleEvent(event) {
     return Promise.resolve(null)
   }
 
-  const humanMessage = event.message.text
-  const aiMessage = await ask(humanMessage)
+  let userMessage = event.message.text
+  const sourceId = event.source.groupId || event.source.userId
+  let groupCanReply = false
 
-  console.log(c.green('[human]: ' + humanMessage))
-  console.log(c.blue('[AI]: ' + aiMessage))
+  // clear cache
+  if (['清除', '清除緩存', 'clear'].includes(event.message.text)) {
+    redis.del(`linebot_user:${sourceId}`)
 
-  // create a echoing text message
-  const echo = { type: 'text', text: aiMessage }
+    return linebot.replyMessage(event.replyToken, {
+      type: 'text',
+      text: '清除完成~',
+    })
+  }
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo)
+  const groupStartKeyword = ['/chat', 'ChatGPT', 'chatgpt']
+  if (event.source.type === 'group') {
+    const keyword = groupStartKeyword.find(text => userMessage.startsWith(text))
+    if (keyword) {
+      userMessage = userMessage.replace(new RegExp(`^${keyword}`), '').trim()
+      groupCanReply = true
+    }
+  }
+
+  // echo message with ChatGPT
+  if (event.source.type === 'user' || groupCanReply) {
+    redis.lPush(`linebot_user:${sourceId}`, userMessage)
+    redis.expire(`linebot_user:${sourceId}`, 60 * 60 * 1) // expires in 1 hour
+    const cacheMessages = await redis.lRange(`linebot_user:${sourceId}`, 0, -1)
+
+    const aiMessage = await ask(cacheMessages, sourceId)
+
+    console.log(c.green(`[user]: ${userMessage}`))
+    console.log(c.blue(`[AI]: ${aiMessage}`))
+
+    // use reply API
+    return linebot.replyMessage(event.replyToken, {
+      type: 'text',
+      text: aiMessage,
+    })
+  }
 }
 
-async function ask(message) {
+async function ask(message, userId) {
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: message,
     max_tokens: 500,
+    user: userId,
   })
 
   return completion.data.choices[0].text.trim()
